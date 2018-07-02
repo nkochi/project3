88095fdee31d7defca9d5ee07c3c889b184605a3
Sean Newman <seangnewman@msn.com>
2018-07-02 01:13:02 -0700

    Implemented okta

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withAuth } from '@okta/okta-react';
 
-export default () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+export default withAuth(class Home extends Component {
+   
+
+  state = {authenticated : null};
+
+   checkAuthentication = async() => {
+    const authenticated = await this.props.auth.isAuthenticated();
+    if (authenticated !== this.state.authenticated) {
+      this.setState({ authenticated });
+    }
+  }
+
+  async componentDidMount() {
+    this.checkAuthentication();
+  }
+
+  async componentDidUpdate() {
+    this.checkAuthentication();
+  }
+
+  login = async() =>  {
+    this.props.auth.login('/');
+  }
+
+logout = ()  => {
+    this.props.auth.logout('/');
+  }
+
+  render() {
+    if (this.state.authenticated === null) return null;
+
+    const mainContent = this.state.authenticated ?
+       (
+         <div>
+          <p className="lead">You have entered the member portal, 
+            <Link to="staff">click here</Link>
+          </p>
+          <button className="btn btn-light btn-lg" onClick={this.logout}>Logout</button>
+         </div>
+       ) :
+       (
+
+        <div>
+          <p className="lead">If you are a member, please enter your credentials
+          </p>
+           <button className="btn btn-dark btn-lg" onClick={this.login}>Logout</button>
+
+        </div>
+       );
+
+    return (
+      <div className="jumbotron">
+       <h1 className="display-4">Project 3 Portal</h1>
+       {mainContent}
+    
+      </div>
+    );
+  }
+});
--- src/components/pages/Staff.js
+++ src/components/pages/Staff.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => {
-  return (
-    <div>
-      <h1>Staff</h1>
-    </div>
-  )
+ class Staff extends Component {
+
+  state = {
+    currentUserName: '',
+    currentUserEmaiil: ''
+  }
+
+  componentDidMount(){
+    const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
+    this.setState({currentUserEmail : idToken.idToken.claims.email,
+                   currentUserName : idToken.idToken.claims.name
+    })
+  }
+
+
+  render() {
+    const {currentUserEmail, currentUserName} = this.state;
+
+    return (
+      
+      <div>
+        <h1> Welcome {currentUserName}</h1>
+        <p> Email : {currentUserEmail} </p>
+        <p>You have reached the authorized user area of portal</p>
+      </div>
+    )
+  }
 }
+
+export default Staff;
