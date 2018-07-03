dbc6f9436a103031b6f099d0a1bd4fa1e5c9434f
Sean Newman <seangnewman@msn.com>
2018-07-02 19:01:45 -0700

    resolved error when attempting logoff

--- src/components/layout/Navbar/Navbar.js
+++ src/components/layout/Navbar/Navbar.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
- class Navbar extends Component {
+class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-      <div className = "container">
-        <Link className="navbar-brand" to="/">
-          Project 3
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            Project 3
         </Link>
-        <button className = "navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className ="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
               </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/staff">
-                Staff
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/staff">
+                  Members
               </Link>
-            </li>
+              </li>
 
-          </ul>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     )
   }
 };
 
 export default Navbar;
- 
 
- 
+
+
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import { withAuth } from '@okta/okta-react';
 
 export default withAuth(class Home extends Component {
-   
 
-  state = {authenticated : null};
 
-   checkAuthentication = async() => {
+  state = { authenticated: null };
+
+  checkAuthentication = async () => {
     const authenticated = await this.props.auth.isAuthenticated();
     if (authenticated !== this.state.authenticated) {
       this.setState({ authenticated });
@@ -22,41 +22,52 @@ export default withAuth(class Home extends Component {
     this.checkAuthentication();
   }
 
-  login = async() =>  {
+  login = async () => {
     this.props.auth.login('/');
   }
 
-logout = ()  => {
-    this.props.auth.logout('/');
+  logout = async () => {
+    let loginRedirect = this.login
+    // Redirect to '/' after logout
+    //if session is active
+    this.props.auth.logout('/')
+      .then(function (out) {
+        console.log("session is deleted");
+      })
+      .catch(function (error) {
+        //if session is inactive
+        loginRedirect();
+      })
   }
+
 
   render() {
     if (this.state.authenticated === null) return null;
 
     const mainContent = this.state.authenticated ?
-       (
-         <div>
-          <p className="lead">You have entered the member portal, 
+      (
+        <div>
+          <p className="lead">You have entered the member portal,
             <Link to="staff">click here</Link>
           </p>
           <button className="btn btn-light btn-lg" onClick={this.logout}>Logout</button>
-         </div>
-       ) :
-       (
+        </div>
+      ) :
+      (
 
         <div>
           <p className="lead">If you are a member, please enter your credentials
           </p>
-           <button className="btn btn-dark btn-lg" onClick={this.login}>Logout</button>
+          <button className="btn btn-dark btn-lg" onClick={this.login}>Logout</button>
 
         </div>
-       );
+      );
 
     return (
       <div className="jumbotron">
-       <h1 className="display-4">Project 3 Portal</h1>
-       {mainContent}
-    
+        <h1 className="display-4">Project 3 Portal</h1>
+        {mainContent}
+
       </div>
     );
   }
